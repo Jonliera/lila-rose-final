04d9ec36e8435906f95e8476825568a792591d43
Jonathan Liera <jonliera1@gmail.com>
2023-05-09 16:20:41 -0700

    add daysiui to the project and configured tailwind.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#bdbff9",
+
+          secondary: "#88ef28",
+
+          accent: "#95f4ce",
+
+          neutral: "#1B232C",
+
+          "base-100": "#253350",
+
+          info: "#54AAC9",
+
+          success: "#43DF99",
+
+          warning: "#F69D28",
+
+          error: "#F45D7E",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
