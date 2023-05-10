c598149737dc5983fd8e4218c218c44ba6e00a0c
Jonathan Liera <jonliera1@gmail.com>
2023-05-09 19:28:04 -0700

    add supabase to the project to handle the database

---
+++ src/types/supabase.ts
@@ -0,0 +1,95 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      hats_info: {
+        Row: {
+          created_at: string | null
+          id: number
+          ImageSrc: string | null
+          Price: string | null
+          Title: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+      }
+      hats_info2: {
+        Row: {
+          created_at: string | null
+          id: number
+          ImageSrc: string | null
+          Price: string | null
+          Title: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+      }
+      hats_info3: {
+        Row: {
+          created_at: string | null
+          id: number
+          ImageSrc: string | null
+          Price: string | null
+          Title: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          ImageSrc?: string | null
+          Price?: string | null
+          Title?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
