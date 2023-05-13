3c8061df5e313e75d8dcf263dae5f2225cbfb514
Jonathan Liera <jonliera1@gmail.com>
2023-05-13 10:58:20 -0700

    add a products page to route back from details

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,12 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="hero max-h-screen bg-base-200">
-      <div className="hero-content text-center mt-10">
-        <div className="max-w-lg">
-          <h1 className="text-5xl font-bold">Welcome to Lila & Rose</h1>
-          <p className="py-6">
+      <div className="hero__content hero-content text-center mt-10">
+        <div className="hero__content-inner max-w-lg">
+          <h1 className="hero__heading text-5xl font-bold">
+            Welcome to Lila & Rose
+          </h1>
+          <p className="hero__intro py-6">
             We are a small business that makes custom hats for all occasions. We
             have a wide variety of styles and colors to choose from. We also
             offer custom embroidery for your hat. We are located in the heart of
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 import About from "@/components/About";
 import Link from "next/link";
+import Login from "@/components/Login";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Products />
       <About />
       <Footer />
+      <Login />
     </>
   );
 }
